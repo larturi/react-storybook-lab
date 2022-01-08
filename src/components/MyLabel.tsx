8544d4b00c8d2e7c4831f5fcb8399634d3e704b9
lea.arturi <lea.arturi@gmail.com>
2022-01-08 16:19:23 -0300

    Custom color

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -1,17 +1,41 @@
 import './MyLabel.css';
 
 export interface MyLabelProps {
+    /**
+    * Este es el texto que se muestra en el label
+    */
     label: string;
+    /**
+    * Este es el tamaño del label
+    */
     size: 'normal' | 'h1' | 'h2' | 'h3';
+    /**
+    * Si esta en true el label se muestra el texto en mayúscula
+    */
+    allCaps?: boolean;
+    /**
+    * Aplica un color al label
+    */
+    color?: 'primary' | 'secondary' | 'tertiary';
+    /**
+    * Aplica el color seleccionado al label
+    */
+    fontColor?: string;
 }
 
 export const MyLabel = ( {
     label = 'No label',
-    size = 'normal'
+    size = 'normal',
+    allCaps = false,
+    color = 'primary',
+    fontColor
 }: MyLabelProps ) => {
     return (
-        <span className={`${size}`}>
-            { label }
+        <span 
+            className={`label ${size} text-${color}`}
+            style={{ color: fontColor }}
+        >
+            { allCaps ? label.toUpperCase() : label }
         </span>
     )
 }
--- src/stories/components/MyLabel.stories.tsx
+++ src/stories/components/MyLabel.stories.tsx
@@ -3,7 +3,37 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 export default {
     title: 'UI/MyLabel',
-    component: MyLabel
+    component: MyLabel,
+    argTypes: {
+        label: {
+            control: {
+                type: 'text'
+            }
+        },
+        size: {
+            control: {
+                type: 'select',
+                options: ['normal', 'h1', 'h2', 'h3']
+            }
+        },
+        allCaps: {
+            control: {
+                type: 'boolean'
+            }
+        },
+        color: {
+            control: {
+                type: 'select',
+                options: ['primary', 'secondary', 'tertiary']
+            }
+        },
+        fontColor: {
+            control: {
+                type: 'color'
+            }
+        }
+    }
+
 } as ComponentMeta<typeof MyLabel>;
 
 const Template: ComponentStory<typeof MyLabel> = (args) => 
@@ -11,9 +41,30 @@ const Template: ComponentStory<typeof MyLabel> = (args) =>
 
 export const Basic = Template.bind({});
 Basic.args = {
-    size: 'normal'
+    size: 'normal',
+    allCaps: false
 }
 
 export const AllCaps = Template.bind({});
+AllCaps.args = {
+    size: 'normal',
+    allCaps: true
+}
 
 export const Secondary = Template.bind({});
+Secondary.args = {
+    size: 'normal',
+    color: 'secondary'
+}
+
+export const Tertiary = Template.bind({});
+Tertiary.args = {
+    size: 'normal',
+    color: 'tertiary'
+}
+
+export const FontColor = Template.bind({});
+FontColor.args = {
+    size: 'h1',
+    fontColor: '#5e3db9' 
+}
